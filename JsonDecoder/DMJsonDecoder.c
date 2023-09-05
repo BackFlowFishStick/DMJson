e51513cd3632dedf88f7fd1c4934eb37a8380e1a
BackFlowFishStick <82761004+BackFlowFishStick@users.noreply.github.com>
2023-09-06 01:03:26 +0800

    few Changes. JsonDecoder need target key.

--- JsonDecoder/DMJsonDecoder.c
+++ JsonDecoder/DMJsonDecoder.c
@@ -1,25 +1,31 @@
 #include "DMJsonDecoder.h"
 
-void get_json_obj(const char* jsonStr, struct JsonObj* obj)
+void get_json_obj(const char* jsonStr, const char* objKey, struct JsonObj* obj)
 {
+
 }
 
-void get_json_int(const char* jsonStr, struct JsonObj* obj)
+void get_json_int(const char* jsonStr, const char* objKey, struct JsonObj* obj)
 {
+
 }
 
-void get_json_float(const char* jsonStr, struct JsonObj* obj)
+void get_json_float(const char* jsonStr, const char* objKey, struct JsonObj* obj)
 {
+
 }
 
-void get_json_bool(const char* jsonStr, struct JsonObj* obj)
+void get_json_bool(const char* jsonStr, const char* objKey, struct JsonObj* obj)
 {
+
 }
 
-void get_json_null(const char* jsonStr, struct JsonObj* obj)
+void get_json_null(const char* jsonStr, const char* objKey, struct JsonObj* obj)
 {
+
 }
 
-void get_json_arr(const char* jsonStr, struct JsonObj* obj)
+void get_json_arr(const char* jsonStr, const char* objKey, struct JsonObj* obj)
 {
+
 }
