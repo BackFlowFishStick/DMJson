ec45205215bc69bd541337acafcf9718cb1da1e9
BackFlowFishStick <82761004+BackFlowFishStick@users.noreply.github.com>
2023-09-06 19:01:25 +0800

    decode_json_str added

--- JsonDecoder/DMJsonDecoder.c
+++ JsonDecoder/DMJsonDecoder.c
@@ -1,7 +1,45 @@
 #include "DMJsonDecoder.h"
 
+uint8_t decode_json_str(const char* jsonStr, struct JsonObj* out) 
+{
+	int i = 0;
+
+	int len = strlen(jsonStr);
+
+	int isBracked = 0; 
+
+	if(len == 0) 
+	{
+		return 0;
+	}
+
+	if(jsonStr[i] != '{' || jsonStr[i] != '[') 
+	{
+		return 0;
+	}
+
+	if(jsonStr[len - 2] != '}' || jsonStr[len - 2] != ']') 
+	{
+		return 0; 
+	}
+	
+	while(jsonStr[i] != '\0') 
+	{
+		i++;
+
+		if(jsonStr[i] = '{')
+	}
+
+}
+
 void get_json_obj(const char* jsonStr, const char* objKey, struct JsonObj* obj)
 {
+	int i = 0;
+
+	while(jsonStr[i] != '{') 
+	{
+		i++;
+	}
 
 }
 
--- JsonDecoder/DMJsonDecoder.h
+++ JsonDecoder/DMJsonDecoder.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "DMJsonEncoder.h"
 
+uint8_t decode_json_str(const char* jsonStr, struct JsonObj* out);
 void get_json_obj(const char* jsonStr, const char* objKey, struct JsonObj* obj);
 void get_json_int(const char* jsonStr, const char* objKey, struct JsonObj* obj);
 void get_json_float(const char* jsonStr, const char* objKey, struct JsonObj* obj);
