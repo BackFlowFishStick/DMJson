f8265a1a2ca279c71dc6910e208f4636c498f5a3
BackFlowFishStick <82761004+BackFlowFishStick@users.noreply.github.com>
2023-09-11 01:39:33 +0800

    Still figuring out how to parse json string

--- JsonDecoder/DMJsonDecoder.c
+++ JsonDecoder/DMJsonDecoder.c
@@ -1,100 +1,112 @@
 #include "DMJsonDecoder.h"
 
-uint8_t decode_json_str_to_obj(const char* jsonStr, struct JsonObj* out) 
+uint8_t decode_json_str_to_obj(const char* json_str, struct json_obj* out) 
 {
-	int i = 0;
+	int inside_braces = 0;
+	int obj_start_index = 0;
+	int obj_end_index = 0;
 
-	const int len = (int)strlen(jsonStr);
+	int inside_quotation = 0;
+	int quotation_start_index = 0;
+	int quotation_end_index = 0;
 
-	int isBracket = 1;
-	int isParantheses = 1;
-	int isKey = 1;
+	initialize_json_obj(out);
 
-	if(len == 0) 
-	{
-		return 0;
-	}
+	for (int i = 1; i < strlen(json_str); i++) {
+		if (json_str[i] == '{') {
 
-	if(jsonStr[0] != '{' && jsonStr[0] != '[') 
-	{
-		return 0;
-	}
+			inside_braces = 1;
 
-	if(jsonStr[len - 2] != '}' && jsonStr[len - 2] != ']') 
-	{
-		return 0; 
-	}
-	
-	while(jsonStr[i] != '\0') 
-	{
-		i++;
+			obj_start_index = i + 1;
 
-		if(jsonStr[i] == '{')
-		{
-			isBracket = 0;
+			struct json_obj child_node;
+			initialize_json_obj(&child_node);
+			const int child_node_result = decode_json_str_to_obj((json_str + obj_start_index), &child_node);
 
-		}
-
-		else if(jsonStr[i] == '[' )
-		{
-			isParantheses = 0;
-		}
-
-		if(jsonStr[i] == '}' && isBracket == 0)
-		{
-			isBracket = 1;
-		}
-		else if(jsonStr[i] == ']' && isParantheses == 0)
-		{
-			isParantheses = 1;
-		}
-		if (jsonStr[i] == '\"')
-		{
-			isKey = 0;
-			
-			for(int j = i + 1; jsonStr[j] != '\"'; ++j)
+			if(child_node_result == 1)
 			{
-				
+				add_attribute(&child_node, out);
 			}
 		}
-		
+		else if (json_str[i] == '}' && inside_braces == 1) 
+		{
+			obj_end_index = i; 
+			break; 
+		}
 	}
 
-	if(isBracket == 0 || isParantheses == 0)
+	if(obj_end_index == 0)
 	{
+		printf("The json object format is not correct.\n");
 		return 0;
 	}
 
+	return 1;
 }
 
-uint8_t get_json_obj(const char* objKey, struct JsonObj* obj)
+uint8_t get_json_obj(const char* obj_key, struct json_obj* obj)
 {
 	
 
 	return 0;
 }
 
-uint8_t get_json_int(const char* objKey, struct JsonObj* obj)
+uint8_t get_json_int(const char* obj_key, struct json_obj* obj)
 {
 	return 0;
 }
 
-uint8_t get_json_float(const char* objKey, struct JsonObj* obj)
+uint8_t get_json_float(const char* obj_key, struct json_obj* obj)
 {
 	return 0;
 }
 
-uint8_t get_json_bool(const char* objKey, struct JsonObj* obj)
+uint8_t get_json_bool(const char* obj_key, struct json_obj* obj)
 {
 	return 0;
 }
 
-uint8_t get_json_null(const char* objKey, struct JsonObj* obj)
+uint8_t get_json_null(const char* obj_key, struct json_obj* obj)
 {
 	return 0;
 }
 
-uint8_t get_json_arr(const char* objKey, struct JsonObj* obj)
+uint8_t get_json_arr(const char* obj_key, struct json_obj* obj)
 {
+	return 0;
+}
+
+uint8_t get_json_obj_num(const char* json_str)
+{
+
+	const int len = (int)strlen(json_str);
+
+	int isBracket = 0;
+	int got_left_curly_brace = 0;
+	int got_right_curly_brace = 0;
+	int is_parantheses = 0;
+	int is_key = 0;
+
+
+	for(int i = 0; json_str[i] != '\0'; ++i)
+	{
+		if ('{' == json_str[i])
+		{
+			got_left_curly_brace += 1;
+		}
+		else if('}' == json_str[i])
+		{
+			got_right_curly_brace += 1;
+		}
+	}
+
+	if(got_left_curly_brace != got_right_curly_brace)
+	{
+		printf("This is not Json format\n");
+		return 0;
+	}
+
+
+
 	return 0;
 }
--- JsonDecoder/DMJsonDecoder.h
+++ JsonDecoder/DMJsonDecoder.h
@@ -1,10 +1,11 @@
 #pragma once
 #include "DMJsonEncoder.h"
 
-uint8_t decode_json_str_to_obj(const char* jsonStr, struct JsonObj* out);
-uint8_t get_json_obj(const char* objKey, struct JsonObj* obj);
-uint8_t get_json_int(const char* objKey, struct JsonObj* obj);
-uint8_t get_json_float(const char* objKey, struct JsonObj* obj);
-uint8_t get_json_bool(const char* objKey, struct JsonObj* obj);
-uint8_t get_json_null(const char* objKey, struct JsonObj* obj);
-uint8_t get_json_arr(const char* objKey, struct JsonObj* obj);
+uint8_t decode_json_str_to_obj(const char* json_str, struct json_obj* out);
+uint8_t get_json_obj(const char* obj_key, struct json_obj* obj);
+uint8_t get_json_int(const char* obj_key, struct json_obj* obj);
+uint8_t get_json_float(const char* obj_key, struct json_obj* obj);
+uint8_t get_json_bool(const char* obj_key, struct json_obj* obj);
+uint8_t get_json_null(const char* obj_key, struct json_obj* obj);
+uint8_t get_json_arr(const char* obj_key, struct json_obj* obj);
+uint8_t get_json_obj_num(const char* json_str);
