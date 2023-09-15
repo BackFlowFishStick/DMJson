81029f86bc7e865d76214f46f66679d2f6cd8c9a
BackFlowFishStick <82761004+BackFlowFishStick@users.noreply.github.com>
2023-09-15 17:40:04 +0800

    few changes

--- JsonDecoder/DMJsonDecoder.c
+++ JsonDecoder/DMJsonDecoder.c
@@ -1,6 +1,6 @@
 #include "DMJsonDecoder.h"
 
-uint8_t decode_json_str_to_obj(const char *json_str, struct json_obj *out)
+uint8_t decode_json_str_to_obj(const char *json_str, struct json_obj *out, int *current_index)
 {
     if(json_str == NULL)
     {
@@ -13,14 +13,21 @@ uint8_t decode_json_str_to_obj(const char *json_str, struct json_obj *out)
         return 0;
     }
 
+    if(current_index == NULL || *current_index < 0 || *current_index > strlen(json_str))
+    {
+        printf("The current index is invalid.\n");
+        return 0;
+    }
 
-	int inside_braces = 0;
+    int inside_braces = 0;
 	int obj_start_index = -1;
 	int obj_end_index = -1;
 
 	int inside_quotation = 0;
-	int quotation_start_index = -1;
-	int quotation_end_index = -1;
+	int key_start_index = -1;
+	int key_end_index = -1;
+    int str_value_start_index = -1;
+    int str_value_end_index = -1;
 
 	int inside_square_bracket = 0;
 	int arr_start_index = -1;
@@ -28,90 +35,126 @@ uint8_t decode_json_str_to_obj(const char *json_str, struct json_obj *out)
 
     int is_obj_finish = 0;
     int is_key_finish = 0;
-	int is_before_colon = 0;
+	int is_after_colon = 0;
 
  
 	initialize_json_obj(out);
 
-	for (int i = 0; i < strlen(json_str); i++) {
-		if (json_str[i] == '{')
+	for (int i = *current_index; i < strlen(json_str); i++)
+    {
+        if(json_str[i] == '\"')
         {
-            inside_braces = 1;
-
-			obj_start_index = i + 1;
-
-		}
-
-		else if(json_str[i] == '[')
-		{
-			inside_square_bracket = 1;
-
-			arr_start_index = i + 1;
-		}
-
-        else if(json_str[i] == '\"')
-        {
-
-        }
-
-		else if(json_str[i] == ':')
-		{
-			if(is_before_colon == 0 && quotation_start_index != -1) 
-			{
-				for(int quoation_index = quotation_start_index; quoation_index < quotation_end_index + 1; quoation_index++)
-				{
-					
-				}
-			}
-		}
-
-		else if(json_str[i] == ']' && inside_square_bracket == 1)
-		{
-			arr_end_index = i - 1;
-
-            inside_square_bracket = 0;
-		}
-
-        else if (json_str[i] == '}')
-        {
-            if(inside_braces == 1)
+            if(inside_quotation == 0)
             {
-                struct json_obj child_node;
+                inside_quotation = 1;
 
-                initialize_json_obj(&child_node);
-
-                const int child_node_result = decode_json_str_to_obj((json_str + obj_start_index), &child_node);
-
-                if(child_node_result == 1)
+                if(is_after_colon != 0)
                 {
-                    obj_end_index = i - 1;
-
-                    add_attribute(&child_node, out);
-
-                    inside_braces = 0;
+                    str_value_start_index = i + 1;
                 }
-                // The end of a json object, so we return 1 to let user know that the function just generated a json object.
-                return 1;
+                else
+                {
+                    key_start_index = i + 1;
+                }
             }
             else
             {
-                // Detect wrong format of json text, so we return 0 to let user know they used wrong format text.
-                printf("The text format is incorrect.\n");
+                inside_quotation = 0;
 
-                return 0;
+                if(is_after_colon != 0)
+                {
+                    str_value_end_index = i - 1;
+                }
+                else
+                {
+                    key_end_index = i - 1;
+                }
+
             }
+        }
+        if(json_str[i] == ':')
+        {
+            if(is_after_colon != 0)
+            {
+                goto invalid_format;
+            }
+            else
+            {
+                is_after_colon = 1;
+            }
+        }
+
+        if(json_str[i] == '{')
+        {
+            if(inside_braces == 0)
+            {
+                obj_start_index = i + 1;
+            }
+            inside_braces += 1;
+            if(obj_start_index > strlen(json_str))
+            {
+                goto invalid_format;
+            }
+        }
+
+        if(json_str[i] == '}')
+        {
+            inside_braces -= 1;
+            if(inside_braces == 0)
+            {
+                obj_end_index = i - 1;
+            }
+            if(obj_end_index < 0)
+            {
+                goto invalid_format;
+            }
+        }
+        if(json_str[i] == '[')
+        {
+            if(inside_square_bracket == 0)
+            {
+                arr_start_index = i + 1;
+            }
+            inside_square_bracket += 1;
+
+            if(arr_start_index > strlen(json_str))
+            {
+                goto invalid_format;
+            }
+        }
+
+        if(json_str[i] == ']')
+        {
+            inside_square_bracket -= 1;
+            if(inside_square_bracket == 0)
+            {
+                arr_end_index = i - 1;
+            }
+            if(arr_end_index < 0)
+            {
+                goto invalid_format;
+            }
+        }
+
+        if(json_str[i] == ',')
+        {
+            struct json_obj child_node;
+            initialize_json_obj(&child_node);
 
 
         }
+
 	}
 
-	if(obj_end_index == 0 || inside_braces != 0 || inside_square_bracket != 0 || inside_quotation != 0)
-	{
-		printf("The json object format is not correct.\n");
-		return 0;
-	}
+    return 1;
 
-	return 1;
+    invalid_format:
+    {
+        printf("The format is invalid.\n");
+        return 0;
+    };
+
+
 }
 
 uint8_t get_json_obj(const char* obj_key, struct json_obj* obj)
@@ -143,41 +186,6 @@ uint8_t get_json_null(const char* obj_key, struct json_obj* obj)
 
 uint8_t get_json_arr(const char* obj_key, struct json_obj* obj)
 {
-	return 0;
-}
-
-uint8_t get_json_obj_num(const char* json_str)
-{
-
-	const int len = (int)strlen(json_str);
-
-	int isBracket = 0;
-	int got_left_curly_brace = 0;
-	int got_right_curly_brace = 0;
-	int is_parantheses = 0;
-	int is_key = 0;
-
-
-	for(int i = 0; json_str[i] != '\0'; ++i)
-	{
-		if ('{' == json_str[i])
-		{
-			got_left_curly_brace += 1;
-		}
-		else if('}' == json_str[i])
-		{
-			got_right_curly_brace += 1;
-		}
-	}
-
-	if(got_left_curly_brace != got_right_curly_brace)
-	{
-		printf("This is not Json format\n");
-		return 0;
-	}
-
-
-
 	return 0;
 }
 
--- JsonDecoder/DMJsonDecoder.h
+++ JsonDecoder/DMJsonDecoder.h
@@ -1,7 +1,7 @@
 #pragma once
 #include "DMJsonEncoder.h"
 
-uint8_t decode_json_str_to_obj(const char *json_str, struct json_obj *out);
+uint8_t decode_json_str_to_obj(const char *json_str, struct json_obj *out, int *current_index);
 struct json_obj* get_current_attribute(const struct json_obj *root);
 uint8_t get_json_obj(const char* obj_key, struct json_obj* obj);
 uint8_t get_json_int(const char* obj_key, struct json_obj* obj);
@@ -9,4 +9,3 @@ uint8_t get_json_float(const char* obj_key, struct json_obj* obj);
 uint8_t get_json_bool(const char* obj_key, struct json_obj* obj);
 uint8_t get_json_null(const char* obj_key, struct json_obj* obj);
 uint8_t get_json_arr(const char* obj_key, struct json_obj* obj);
-uint8_t get_json_obj_num(const char* json_str);
